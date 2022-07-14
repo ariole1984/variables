b020fd48ba4724496bbb2bb369ac8104d9f3f7be
Ariole Dieujuste <arioledieujuste@gmail.com>
2022-07-14 12:11:53 -0400

    Just a folder

---
+++ lab.js
@@ -0,0 +1,13 @@
+let favColor = 'blue'
+console.log(favColor);
+
+let countBag = 5
+let countShoe = 4
+
+const myName = "Ariole"
+const sumTotal = myName * countBag
+console.log(sumTotal)
+
+let truthIsOutThere = true
+let falselsNotGood = false
+console.log(truthIsOutThere + falseIsNotGood)
---
+++ variables.js
@@ -0,0 +1,16 @@
+//first comment 
+/* second command
+in multible lines
+*/
+
+let myAge = 38
+console.log(`my age is  , ${myAge}" , 
+
+
+
+
+hello`);
+
+// console.log('My name is Ariole');
+// console.log("My name is Ariole");
+// console.log(`My name is Ariole`);
